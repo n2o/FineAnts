66c48ce23a37ad0634578f353078437c91562a20
n2o <christian.meter@uni-duesseldorf.de>
2015-12-23 00:59:59 +0100

    Define transaction model

--- fineants/models.py
+++ fineants/models.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    title = models.CharField("Title", max_length=255, blank=False)
+    creditor = models.ForeignKey(User, verbose_name="Creditor", related_name="Creditor")
+    debitors = models.ManyToManyField(User, verbose_name="Debitors", related_name="Debitors")
+    amount = models.FloatField("Amount", blank=False)
+    created = models.DateTimeField('Created', default=datetime.now)
