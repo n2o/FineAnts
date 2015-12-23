61d6fedb79b9ededdc1ae964eea94aa1d5b4a64c
n2o <christian.meter@uni-duesseldorf.de>
2015-12-23 01:00:15 +0100

    Configure admin panel for fineants app

--- fineants/admin.py
+++ fineants/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creditor', 'amount', 'created')
+    search_fields = ['title', 'creditor', 'amount', 'created']
+    fieldsets = [
+        ('General', {'fields': ['title', 'creditor', 'debitors', 'amount']}),
+        ('Meta',    {'fields': ['created'], 'classes': ['collapse']}),
+    ]
+
+
+admin.site.register(Transaction, TransactionAdmin)
