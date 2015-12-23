ce4b4367f893f5240a5a27dcf7fc47dea6ba2d10
n2o <christian.meter@uni-duesseldorf.de>
2015-12-23 01:29:23 +0100

    Define first simple view

--- fineants/views.py
+++ fineants/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Transaction
 
-# Create your views here.
+
+def list_transactions(request):
+    transactions = get_list_or_404(Transaction.objects.order_by('created'))
+    return render(request, 'fineants/list_transactions.html', {'transactions': transactions})
